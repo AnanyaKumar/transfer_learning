abf51431d2fee857d76420c9a2fdc1c8427676df
Ananya Kumar <skywalker94@gmail.com>
2021-03-31 20:03:57 -0700

    Add support for moco initialization.

--- models/imnet_resnet.py
+++ models/imnet_resnet.py
@@ -1,13 +1,42 @@
 
+import torchvision.models as models
 from torchvision.models import resnet50
 import torch
 from torch import nn
 
+PRETRAIN_STYLE = ['supervised', 'mocov2', 'simclrv2']
+
+
+def load_moco(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    model = models.__dict__[checkpoint['arch']]()
+
+    state_dict = checkpoint['state_dict']
+    for k in list(state_dict.keys()):
+        # retain only encoder_q up to before the embedding layer
+        if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
+            # remove prefix
+            state_dict[k[len("module.encoder_q."):]] = state_dict[k]
+        # delete renamed or unused k
+        del state_dict[k]
+    msg = model.load_state_dict(state_dict, strict=False)
+    assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
+    return model
+
+
 class ResNet50(nn.Module):
 
-    def __init__(self, pretrained=False):
+    def __init__(self, pretrained=False, pretrain_style='supervised', checkpoint_path=None):
         super().__init__()
-        self._model = resnet50(pretrained=pretrained)
+        if pretrain_style not in PRETRAIN_STYLE:
+            raise ValueError(
+                f'Pretrain style should be in {PRETRAIN_STYLE} but was {pretrain_style}')
+        if pretrained and pretrain_style == 'mocov2':
+            self._model = load_moco(checkpoint_path)
+        elif pretrained and pretrain_style == 'simclrv2':
+            raise NotImplementedError()
+        elif not pretrained or pretrain_style == 'supervised':
+            self._model = resnet50(pretrained=pretrained)
 
     def forward(self, x):
         return self._model(x)
