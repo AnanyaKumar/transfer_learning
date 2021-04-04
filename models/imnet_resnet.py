53b3550619ad3d4b86ebefd61ef903aafbab5b09
Ananya Kumar <skywalker94@gmail.com>
2021-04-03 21:17:55 -0700

    Add support for swav.

--- models/imnet_resnet.py
+++ models/imnet_resnet.py
@@ -1,10 +1,13 @@
 
+from collections import OrderedDict
 import torchvision.models as models
 from torchvision.models import resnet50
 import torch
 from torch import nn
+from models import swav_resnet50
 
-PRETRAIN_STYLE = ['supervised', 'mocov2', 'simclrv2']
+
+PRETRAIN_STYLE = ['supervised', 'mocov2', 'swav', 'simclrv2']
 
 
 def load_moco(checkpoint_path):
@@ -24,6 +27,26 @@ def load_moco(checkpoint_path):
     return model
 
 
+def load_swav(checkpoint_path):
+    model = swav_resnet50.resnet50(output_dim=0, eval_mode=False)
+    state_dict = torch.load(checkpoint_path, map_location="cpu")
+    if "state_dict" in state_dict:
+        state_dict = state_dict["state_dict"]
+    # remove prefix "module."
+    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+    for k, v in model.state_dict().items():
+        if k not in list(state_dict):
+            print('key "{}" could not be found in provided state dict'.format(k))
+        elif state_dict[k].shape != v.shape:
+            print('key "{}" is of different shape in model and provided state dict'.format(k))
+            state_dict[k] = v
+    msg = model.load_state_dict(state_dict, strict=False)
+    model = torch.nn.Sequential(OrderedDict([
+        ('resnet', model),
+        ('fc', nn.Linear(2048, 1000))]))
+    return model
+
+
 class ResNet50(nn.Module):
 
     def __init__(self, pretrained=False, pretrain_style='supervised', checkpoint_path=None):
@@ -33,6 +56,8 @@ class ResNet50(nn.Module):
                 f'Pretrain style should be in {PRETRAIN_STYLE} but was {pretrain_style}')
         if pretrained and pretrain_style == 'mocov2':
             self._model = load_moco(checkpoint_path)
+        if pretrained and pretrain_style == 'swav':
+            self._model = load_swav(checkpoint_path)
         elif pretrained and pretrain_style == 'simclrv2':
             raise NotImplementedError()
         elif not pretrained or pretrain_style == 'supervised':
