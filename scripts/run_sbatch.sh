57b02bd4b525b7b78222029c64f49f2351a0ec3a
Ananya Kumar <skywalker94@gmail.com>
2021-04-27 23:14:06 -0700

    Improve run_sbatch.

--- scripts/run_sbatch.sh
+++ scripts/run_sbatch.sh
@@ -2,12 +2,16 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
-#SBATCH --exclude=jagupard[4-8],jagupard[10-15]
+#SBATCH --exclude=jagupard[4-8]
+#SBATCH --mem=24G
 
 # Print execute commands in the log.
 set -x
+conda_env=`whoami`-ue
 
-conda activate `whoami`-ue
+source /u/nlp/anaconda/main/anaconda3/etc/profile.d/conda.sh
+conda deactivate
+conda activate $conda_env
 cd $PWD
 
 eval $1
