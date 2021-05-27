07063425a104c76cf6cc8901479f9a7de43c371d
Ananya Kumar <skywalker94@gmail.com>
2021-05-26 17:42:40 -0700

    Fix for jags4-8

--- scripts/run_sbatch.sh
+++ scripts/run_sbatch.sh
@@ -2,14 +2,14 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
-#SBATCH --exclude=jagupard[4-8]
 #SBATCH --mem=24G
+# #SBATCH --exclude=jagupard[10-25]
 
 # Print execute commands in the log.
 set -x
 conda_env=`whoami`-ue
 
-source copy_imagenet_local.sh
+# source scripts/copy_imagenet_local.sh
 source /u/nlp/anaconda/main/anaconda3/etc/profile.d/conda.sh
 conda deactivate
 conda activate $conda_env
