fcc620620ac91ca35e97cdea784ad4206a601642
Robbie Jones <robbiejones96@gmail.com>
2021-08-06 11:41:30 -0700

    Add loop over all models to CLIP script

--- scripts/launch_clip_experiments_sbatch.sh
+++ scripts/launch_clip_experiments_sbatch.sh
@@ -2,13 +2,63 @@
 
 print_usage () {
     USAGE_STRING="launch_clip_experiments_sbatch.sh [-t|--translate_features]"
-    USAGE_STRING="$USAGE_STRING [--no_dependency]"
+    USAGE_STRING="$USAGE_STRING [--slurm_dependency JOBID]"
     USAGE_STRING="$USAGE_STRING experiment_type model"
     USAGE_STRING="$USAGE_STRING [experiment_args]"
     echo $USAGE_STRING
 }
 
+launch_job () {
+    python_args="$experiment_type $model"
+    if [ $translate_features ]; then
+	python_args+=" --translate_features"
+    fi
+    if [ "$experiment_args" ]; then
+	python_args+=" $experiment_args"
+    fi
+
+    if [ $translate_features ]; then
+	num_tasks=$((NUM_DOMAINS ** 2))
+    else
+	num_tasks=$NUM_DOMAINS
+    fi
+
+    sbatch_cmd="sbatch --array=0-$((num_tasks - 1))"
+    if [ "$slurm_dependency" ]; then
+	sbatch_cmd+=" --dependency=afterany:$slurm_dependency"
+    fi
+    if [ "$big_jags" ]; then
+	sbatch_cmd+=" --exclude jagupard[10-25]"
+    else
+	sbatch_cmd+=" --exclude jagupard[26-29]"
+    fi
+    sbatch_cmd="$sbatch_cmd --parsable $SBATCH_SCRIPT '$python_args'"
+    echo $sbatch_cmd
+    job_number=$(eval $sbatch_cmd)
+    slurm_dependency=$job_number
+    echo "Submitted batch job $job_number"
+    email_cmd="source /u/nlp/anaconda/main/anaconda3/etc/profile.d/conda.sh"
+    email_cmd="$email_cmd; conda deactivate; conda activate $(whoami)-ue"
+
+    aggregate_cmd="python aggregate_clip_results.py $experiment_type $model"
+    if [ $translate_features ]; then
+	aggregate_cmd+=" ${DOMAINS[@]} --translate_features"
+    else
+	aggregate_cmd+=" all"
+    fi
+
+    if [ "$experiment_args" ]; then
+	experiment_kwargs=${experiment_args//-}
+	aggregate_cmd+=" --experiment_kwargs ${experiment_kwargs//-}"
+    fi
+
+    email_cmd+="; $aggregate_cmd"
+    ./send_email_sbatch.sh --slurm_dependency $job_number \
+			   "CLIP Results: $python_args" command "$email_cmd"
+}
+
 DOMAINS=(real clipart painting sketch)
+MODELS=(RN50 RN101 RN50x4 RN50x16 ViT-B/32 ViT-B/16)
 NUM_DOMAINS=${#DOMAINS[@]}
 SBATCH_SCRIPT="run_clip_experiment_array.sh"
 
@@ -18,8 +68,17 @@ while true; do
 	-t|--translate_features)
 	    translate_features=true
 	    ;;
-	--no_dependency)
-	    no_dependency=true
+	--slurm_dependency)
+	    if [ "$2" ]; then
+		slurm_dependency="$2"
+		shift
+	    else
+		echo '--slurm_dependency must be non-empty!'
+		exit 1
+	    fi
+	    ;;
+	--big_jags)
+	    big_jags=true
 	    ;;
 	-*)
 	    echo "Unsupported argument $1"
@@ -32,7 +91,7 @@ while true; do
     shift
 done
 
-if [ $# -lt 2 ]; then
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
     print_usage
     exit 1
 fi
@@ -41,41 +100,11 @@ experiment_type=$1
 model=$2
 experiment_args=$3
 
-echo $experiment_type
-echo $model
-
-python_args="$experiment_type $model"
-if [ $translate_features ]; then
-    python_args+=" --translate_features"
-fi
-if [ "$experiment_args" ]; then
-    python_args+=" $experiment_args"
-fi
-
-if [ $translate_features ]; then
-    num_tasks=$((NUM_DOMAINS ** 2))
+if [ "$model" = all ]; then
+    for model in ${MODELS[@]}; do
+	launch_job
+    done
 else
-    num_tasks=$NUM_DOMAINS
+    launch_job
 fi
 
-sbatch_cmd="sbatch --array=0-$((num_tasks - 1))"
-sbatch_cmd="$sbatch_cmd --parsable $SBATCH_SCRIPT '$python_args'"
-job_number=$(eval $sbatch_cmd)
-
-email_cmd="source /u/nlp/anaconda/main/anaconda3/etc/profile.d/conda.sh"
-email_cmd="$email_cmd; conda deactivate; conda activate $(whoami)-ue"
-
-aggregate_cmd="python aggregate_clip_results.py $experiment_type $model"
-if [ $translate_features ]; then
-    aggregate_cmd+=" ${DOMAINS[@]} --translate_features"
-else
-    aggregate_cmd+=" all"
-fi
-
-if [ "$experiment_args" ]; then
-    aggregate_cmd+=" --experiment_kwargs ${experiment_args//-}"
-fi
-
-email_cmd+="; $aggregate_cmd"
-./send_email_sbatch.sh --slurm_dependency $job_number \
-		       "CLIP Results: $python_args" command "$email_cmd"
