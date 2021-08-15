9c2ab6faed860f03e4b44e838989d5f8134390be
Ananya Kumar <skywalker94@gmail.com>
2021-08-15 12:21:53 +0800

    Add resize transform

---
+++ unlabeled_extrapolation/datasets/transforms.py
@@ -0,0 +1,15 @@
+# Custom transforms.
+
+import torchvision.transforms
+
+class Resize(torchvision.transforms.Resize):
+
+    def __init__(self, size, interpolation='bilinear', max_size=None, antialias=None):
+        if interpolation == 'bilinear':
+            interp_arg = torchvision.transforms.InterpolationMode.BILINEAR
+        if interpolation == 'bicubic':
+            interp_arg = torchvision.transforms.InterpolationMode.BICUBIC
+        if interpolation == 'nearest':
+            interp_arg = torchvision.transforms.InterpolationMode.NEAREST
+        super().__init__(size=size, interpolation=interp_arg, max_size=max_size,
+                         antialias=antialias)
