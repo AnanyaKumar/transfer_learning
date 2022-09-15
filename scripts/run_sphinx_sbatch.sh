87201c516ce4b97406b9b8ed0abbc645b6ce00d1
Ananya Kumar <skywalker94@gmail.com>
2022-09-15 14:24:58 -0700

    Add more wilds support

--- scripts/run_sphinx_sbatch.sh
+++ scripts/run_sphinx_sbatch.sh
@@ -4,11 +4,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=24G
 #SBATCH --partition=sphinx
-#SBATCH -t 2-0:00
 
 # Print execute commands in the log.
 set -x
-conda_env=`whoami`-ue
+conda_env=`whoami`-ue-new
 
 # source scripts/copy_imagenet_local.sh
 source /u/nlp/anaconda/main/anaconda3/etc/profile.d/conda.sh
